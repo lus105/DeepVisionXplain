e0850a61e60d34d1cc32bb2e825a21f8aa6be51d
Lukas_Zabulis <zabulis.lukas@gmail.com>
2024-11-06 19:30:35 +0200

    Add gpu metadata logging

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from src.utils.instantiators import instantiate_callbacks, instantiate_loggers
-from src.utils.logging_utils import log_hyperparameters
+from src.utils.logging_utils import log_hyperparameters, log_gpu_memory_metadata
 from src.utils.pylogger import RankedLogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
 from src.utils.utils import (
