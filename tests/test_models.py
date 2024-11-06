da1e946b8fa7471fe0d09d4fcc1acfd5add8999a
Lukas_Zabulis <zabulis.lukas@gmail.com>
2024-11-07 00:37:52 +0200

    Basic model testing

---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+import hydra
+import omegaconf
+import torch
+import pytest
+
+@pytest.mark.parametrize("num_classes", [1, 10])
+def test_simple_net(num_classes: int):
+    cfg = {
+        "_target_": "src.models.components.simple_dense_net.SimpleDenseNet",
+        "output_size": num_classes
+    }
+    cfg = omegaconf.OmegaConf.create(cfg)
+    model = hydra.utils.instantiate(cfg)
+    model.eval()
+
+    batch_size = 2
+    tensor = torch.randn((batch_size, 1, 28, 28))
+    output = model.forward(tensor)
+
+    assert output.size(dim=0) == batch_size
+    assert output.size(dim=1) == num_classes
