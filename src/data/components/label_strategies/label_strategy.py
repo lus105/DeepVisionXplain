3b4502e742c75627ec6693cbf41942e2d5033246
Lukas_Zabulis <zabulis.lukas@gmail.com>
2024-11-07 21:07:58 +0200

    Delete files

--- src/data/components/label_strategies/label_strategy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import numpy as np
-
-
-class LabelStrategy(ABC):
-    @abstractmethod
-    def process_label(self, label_path: str, image_shape: tuple) -> np.array:
-        pass
