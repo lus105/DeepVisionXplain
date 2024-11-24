acd350eb58ba15aa296affdf61999ea7467bc4c0
Lukas_Zabulis <zabulis.lukas@gmail.com>
2024-11-24 18:54:42 +0200

    Save only weights of the model

--- src/train.py
+++ src/train.py
@@ -73,7 +73,7 @@ def train(cfg: DictConfig) -> tuple[dict[str, Any], dict[str, Any]]:
 
     if cfg.get("train"):
         log.info("Starting training!")
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+        trainer.fit(model=model, datamodule=datamodule)
 
     train_metrics = trainer.callback_metrics
 
