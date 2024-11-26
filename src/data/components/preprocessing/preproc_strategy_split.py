ca5ef95d40d414cc5c4e86e8fe0a5706ad4c515b
Lukas_Zabulis <zabulis.lukas@gmail.com>
2024-11-26 19:32:06 +0200

    Data related fixes

--- src/data/components/preprocessing/preproc_strategy_split.py
+++ src/data/components/preprocessing/preproc_strategy_split.py
@@ -37,23 +37,24 @@ class SplitStep(PreprocessingStep):
         last_subdir = data_path.name
         output_path = base_path / f'{last_subdir}_processed'
 
+        new_data = {
+            self._train_subdir: output_path / self._train_subdir,
+            self._test_subdir: output_path / self._test_subdir,
+            self._val_subdir: output_path / self._val_subdir
+        }
+
         if output_path.exists():
             if overwrite:
                 clear_directory(output_path)
                 output_path.rmdir()
             else:
                 log.info('Data is already split. To overwrite, set data.overwrite=True')
-                return
+                return new_data
         
         dataset_type = self._determine_dataset_type(data_path)
         data_frame = self._to_dataframe(data_path, dataset_type)
         data_frame_splitted = self._split_dataset(data_frame)
         self._save_split(data_frame_splitted, output_path, dataset_type)
-        new_data = {
-            self._train_subdir: output_path / self._train_subdir,
-            self._test_subdir: output_path / self._test_subdir,
-            self._val_subdir: output_path / self._val_subdir
-        }
         return new_data
 
     def _determine_dataset_type(self, data_path: Path) -> DatasetType:
