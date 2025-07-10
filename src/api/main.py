54bcaa618374ea58ad42074b9a0cd4a1ed82d7ed
Lukas_Zabulis <zabulis.lukas@gmail.com>
2025-07-10 21:17:51 +0300

    Delete comment

--- src/api/main.py
+++ src/api/main.py
@@ -3,5 +3,4 @@ from src.api.training import router
 
 
 app = FastAPI(title="NN Training API", description="API for managing training processes.")
-# Include API routes
 app.include_router(router.router, prefix="/training", tags=["Training"])
