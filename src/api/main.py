d1a6dd0bc926486472ab9527ba796336d809472c
Lukas_Zabulis <zabulis.lukas@gmail.com>
2025-08-06 10:50:19 +0300

    Delete middleware

--- src/api/main.py
+++ src/api/main.py
@@ -1,31 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from src.api.training import router
-
 
 app = FastAPI(
     title='NN Training API', description='API for managing training processes.'
 )
 
-# Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
-    ],  # Only allow same origin
-    allow_credentials=True,
-    allow_methods=['GET', 'POST'],  # Only allow needed methods
-    allow_headers=['*'],  # Allows all headers
-)
-
-
 @app.get('/ping')
 async def ping():
     """Health check endpoint"""
     return {'status': 'ok'}
-
 
 app.include_router(router.router, prefix='/training', tags=['Training'])
 
